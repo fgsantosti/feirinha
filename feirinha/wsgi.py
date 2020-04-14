0b84cc455eb289d038574266f27e0406c0859b5f
fgsantosti <fgsantos.ti@gmail.com>
2020-04-13 21:28:55 -0300

    Update wsgi.py

--- feirinha/wsgi.py
+++ feirinha/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ferinha.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'feirinha.settings')
 
 application = get_wsgi_application()
